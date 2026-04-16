73e62053b9cfe9de860c3b786fb9e14242408224
andrey_velsk <andrey_velsk@mail.ru>
2026-04-16 09:56:48 +0300

    update weapon icons

--- src/shared/lib/constants/weaponIcons.ts
+++ src/shared/lib/constants/weaponIcons.ts
@@ -1,16 +1,15 @@
 import { WEAPON_TYPES, type WeaponType } from '@/stores/inventory/types';
 
-// Таблица соответствия типов оружия с относительными путями иконок
 export const WEAPON_ICON_PATHS: Record<Exclude<WeaponType, null>, string> = {
   [WEAPON_TYPES.ONE_HAND_SWORD]: 'lorc/piercing-sword.svg',
   [WEAPON_TYPES.ONE_HAND_DAGGER]: 'lorc/plain-dagger.svg',
   [WEAPON_TYPES.ONE_HAND_AXE]: 'lorc/battered-axe.svg',
   [WEAPON_TYPES.ONE_HAND_MACE]: 'lorc/spiked-mace.svg',
-  [WEAPON_TYPES.TWO_HAND_SWORD]: 'lorc/crossed-swords.svg',
+  [WEAPON_TYPES.TWO_HAND_SWORD]: 'lorc/pointy-sword.svg',
   [WEAPON_TYPES.TWO_HAND_AXE]: 'lorc/battle-axe.svg',
   [WEAPON_TYPES.BOW]: 'lorc/pocket-bow.svg',
   [WEAPON_TYPES.STAFF]: 'lorc/wizard-staff.svg',
-  [WEAPON_TYPES.CROSSBOW]: 'lorc/bowman.svg',
+  [WEAPON_TYPES.CROSSBOW]: 'carl-olsen/crossbow.svg',
   [WEAPON_TYPES.HAND_TO_HAND]: 'lorc/mailed-fist.svg',
 };
 
