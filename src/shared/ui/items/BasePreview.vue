55ae074316820974397db20d2a5b6900a1a47632
andrey_velsk <andrey_velsk@mail.ru>
2026-04-27 10:53:39 +0300

    add stolen icon

--- src/shared/ui/items/BasePreview.vue
+++ src/shared/ui/items/BasePreview.vue
@@ -7,7 +7,19 @@
       v-if="$slots.icon"
       class="d-flex justify-center"
     >
-      <slot name="icon" />
+      <div class="relative">
+        <slot name="icon" />
+        <div
+          v-if="isStolen"
+          class="state-icons"
+        >
+          <base-icon
+            icon-path="sbed/hand.svg"
+            :size="14"
+            class="steal-icon"
+          />
+        </div>
+      </div>
     </div>
     <div class="info">
       <div class="name">
@@ -46,12 +58,14 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { BaseIcon } from '@/shared/ui';
 import { getEffectHtml } from '@/shared/lib/utils/getEffectHtml';
 import type { ItemEnchantmentEffect } from '@/shared/lib/types/common';
 import type { ListItem } from '@/shared/lib/types/types';
 import type { PreviewStats } from './types/types';
 
-withDefaults(
+const props = withDefaults(
   defineProps<{
     data?: ListItem | null;
     stats?: PreviewStats[];
@@ -63,6 +77,10 @@ withDefaults(
     effects: () => [],
   }
 );
+
+const isStolen = computed(() => {
+  return 'isStolen' in props.data! && props.data!.isStolen;
+});
 </script>
 
 <style scoped lang="scss">
@@ -72,6 +90,16 @@ withDefaults(
   height: 100%;
   max-height: 100%;
   min-height: 0;
+
+  .state-icons {
+    position: absolute;
+    bottom: -0.25rem;
+    right: -0.25rem;
+
+    :deep(.steal-icon.base-icon) {
+      background-color: var(--color-danger);
+    }
+  }
 
   .info {
     flex: 0 0 auto;
