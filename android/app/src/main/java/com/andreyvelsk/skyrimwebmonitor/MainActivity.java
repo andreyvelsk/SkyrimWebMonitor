f1db0531009d9bca84f35e46d9470b1081965247
andrey_velsk <andrey_velsk@mail.ru>
2026-05-27 17:02:58 +0300

    fix build apk

--- android/app/src/main/java/com/andreyvelsk/skyrimwebmonitor/MainActivity.java
+++ android/app/src/main/java/com/andreyvelsk/skyrimwebmonitor/MainActivity.java
@@ -1,7 +1,9 @@
 package com.andreyvelsk.skyrimwebmonitor;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
@@ -11,10 +13,22 @@ public class MainActivity extends BridgeActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         super.onCreate(savedInstanceState);
         getBridge().getWebView().setBackgroundColor(Color.parseColor("#0d0d0d"));
+        extendBehindCutout();
         hideSystemBars();
+    }
+
+    private void extendBehindCutout() {
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            params.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            params.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+        getWindow().setAttributes(params);
     }
 
     @Override
