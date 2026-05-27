3a556a3bfd482a5785bf18d2fd5fd33ae6138e91
andrey_velsk <andrey_velsk@mail.ru>
2026-05-27 15:02:57 +0300

    fix(android): white stripes

--- android/app/src/main/java/com/andreyvelsk/skyrimwebmonitor/MainActivity.java
+++ android/app/src/main/java/com/andreyvelsk/skyrimwebmonitor/MainActivity.java
@@ -1,5 +1,34 @@
 package com.andreyvelsk.skyrimwebmonitor;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideSystemBars();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemBars();
+        }
+    }
+
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(
+            getWindow(), getWindow().getDecorView()
+        );
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.setSystemBarsBehavior(
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+    }
+}
