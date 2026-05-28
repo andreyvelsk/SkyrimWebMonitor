18cc97fe9e84fd3de2a1ab0f4258d8e677c04782
andrey_velsk <andrey_velsk@mail.ru>
2026-05-28 13:03:26 +0300

    fix(build): apk

--- android/app/src/main/java/com/andreyvelsk/skyrimwebmonitor/MainActivity.java
+++ android/app/src/main/java/com/andreyvelsk/skyrimwebmonitor/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         getBridge().getWebView().setBackgroundColor(Color.parseColor("#0d0d0d"));
         extendBehindCutout();
         hideSystemBars();
