6c80e3e7a67a2684d10668cb15d31a8aff925fc2
andrey_velsk <andrey_velsk@mail.ru>
2026-08-02 23:16:31 +0300

    cicd tests fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    __USED_ICON_DATA_URLS__: JSON.stringify({}),
   },
   test: {
     environment: 'happy-dom',
     globals: true,
-    include: ['src/**/tests/*.{test,spec}.{ts,js}'],
-    setupFiles: ['src/tests/setup.ts'],
+    include: ['src/**/*.{test,spec}.{ts,js}'],
+    setupFiles: ['./src/tests/setup.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov', 'html'],
